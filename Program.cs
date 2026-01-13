f1e83a33752969fb491dbe3f141e7f54d3a85495
hemanjali0519 <161578217+hemanjali0519@users.noreply.github.com>
2026-01-13 18:41:36 +0530

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,17 +1,2 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Demo1HelloWorld
-{
-    internal class Program
-    {
-        static void Main(string[] args)
-        {
-            //Main method: Entry point of the application.
-            Console.WriteLine("Hello World!");
-        }
-    }
-}
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
